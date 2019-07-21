74d74ed3cd3c127a6e4a9fac4a224973256450c6
Mario Rivera Garcia <cruz.riga@hotmail.com>
2019-07-21 12:48:23 -0500

    agregando el header y esttios
    usa framework materialize

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component, Fragment} from 'react';
+import Header from './components/Header';
+class App extends Component {
+  state = {
+    noticias : []
+  };
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  async componentDidMount(){
+    this.consultarNoticias();
+  }
+
+  consultarNoticias = async ()=>{
+    const ApiKey = "e72093ddcc2c4126905a4ba6750d2ce8";
+    const url = `https://newsapi.org/v2/top-headlines?country=co&category=business&apiKey=${ApiKey}`;
+    const respuesta  = await fetch(url);
+    const noticias = await respuesta.json();
+
+    this.setState({noticias: noticias.articles})    
+  }
+
+  render(){
+    return (
+      <Fragment >
+        <Header titulo="Noticias React API" />
+        <div className="container white contenedor-noticias">
+          
+        </div>
+
+      </Fragment>
+    );
+  }
 }
 
 export default App;
